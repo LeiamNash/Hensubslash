b7c6cb71194beeb7eedb9f3e314caad94cecfc84
LeiamNash <94122613+LeiamNashRebirth@users.noreply.github.com>
2025-07-19 08:22:08 +0000

    function setting on full screen

--- app/src/main/java/com/leiamnash/hensubslash/MainActivity.java
+++ app/src/main/java/com/leiamnash/hensubslash/MainActivity.java
@@ -1,6 +1,5 @@
 package com.leiamnash.hensubslash;
 
-
 import android.content.DialogInterface;
 import android.net.http.SslError;
 import android.support.v7.app.AlertDialog;
@@ -8,107 +7,110 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient.CustomViewCallback;
 import android.graphics.Bitmap;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     String ShowOrHideWebViewInitialUse = "show";
-    private WebView webview ;
+    private WebView webview;
     private ProgressBar spinner;
-    String myurl = "https://leiamnashprojectofhentaigen.vercel.app/home"; 
+    String myurl = "https://leiamnashprojectofhentaigen.vercel.app/home";
+
+    private View mCustomView;
+    private FrameLayout mFullscreenContainer;
+    private CustomViewCallback mCustomViewCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webview =(WebView)findViewById(R.id.webView);
-        spinner = (ProgressBar)findViewById(R.id.progressBar1);
+        webview = (WebView) findViewById(R.id.webView);
+        spinner = (ProgressBar) findViewById(R.id.progressBar1);
+
         webview.setWebViewClient(new CustomWebViewClient());
 
-webview.setWebChromeClient(new WebChromeClient() {
- private View mCustomView;
- private CustomViewCallback mCustomViewCallback;
- protected FrameLayout mFullscreenContainer;
-    
-    @Override
-    public void onShowCustomView(View view, CustomViewCallback callback) {
-    if (mCustomView != null) {
-        callback.onCustomViewHidden();
-            return;
-    }
-        mCustomView = view;
-        mCustomViewCallback = callback;
-        mFullscreenContainer = new FrameLayout(MainActivity.this);
-        mFullscreenContainer.addView(view, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        ViewGroup decor = (ViewGroup) getWindow().getDecorView();
-    decor.addView(mFullscreenContainer);
-        webview.setVisibility(View.GONE);
-getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-}
-    @Override
-    public void onHideCustomView() {
-     if (mCustomView == null) return;
-        ViewGroup decor = (ViewGroup) getWindow().getDecorView();
-        decor.removeView(mFullscreenContainer);
-        mFullscreenContainer = null;
-        mCustomView = null;
-        mCustomViewCallback.onCustomViewHidden();
-        webview.setVisibility(View.VISIBLE);
-getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
- }
-});
-webview.setOnLongClickListener(ignore -> true);
-webview.setLongClickable(false);
-webview.getSettings().setJavaScriptEnabled(true);
-webview.getSettings().setMediaPlaybackRequiresUserGesture(false);
-webview.getSettings().setDomStorageEnabled(true);
-webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onShowCustomView(View view, CustomViewCallback callback) {
+                if (mCustomView != null) {
+                    callback.onCustomViewHidden();
+                    return;
+                }
+
+                mCustomView = view;
+                mCustomViewCallback = callback;
+
+                mFullscreenContainer = new FrameLayout(MainActivity.this);
+                mFullscreenContainer.addView(view, new FrameLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                ));
+
+                ViewGroup decor = (ViewGroup) getWindow().getDecorView();
+                decor.addView(mFullscreenContainer);
+
+                webview.setVisibility(View.GONE);
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            }
+
+            @Override
+            public void onHideCustomView() {
+                if (mCustomView == null) return;
+
+                ViewGroup decor = (ViewGroup) getWindow().getDecorView();
+                decor.removeView(mFullscreenContainer);
+                mFullscreenContainer = null;
+                mCustomView = null;
+                mCustomViewCallback.onCustomViewHidden();
+
+                webview.setVisibility(View.VISIBLE);
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            }
+        });
+
+        webview.setOnLongClickListener(ignore -> true);
+        webview.setLongClickable(false);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
         webview.loadUrl(myurl);
-
     }
 
-
-    
     private class CustomWebViewClient extends WebViewClient {
 
         @Override
         public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
-
             final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-
             builder.setMessage(R.string.notification_error_ssl_cert_invalid);
-
             builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
-
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-
                     handler.proceed();
                 }
             });
-
             builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-
                     handler.cancel();
                 }
             });
-
             final AlertDialog dialog = builder.create();
-
             dialog.show();
         }
 
         @Override
         public void onPageStarted(WebView webview, String url, Bitmap favicon) {
-
             if (ShowOrHideWebViewInitialUse.equals("show")) {
                 webview.setVisibility(webview.INVISIBLE);
             }
@@ -116,24 +118,18 @@ webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
 
         @Override
         public void onPageFinished(WebView view, String url) {
-
             ShowOrHideWebViewInitialUse = "hide";
             spinner.setVisibility(View.GONE);
-
             view.setVisibility(webview.VISIBLE);
             super.onPageFinished(view, url);
-
         }
 
         @Override
-        public void onReceivedError(WebView view, int errorCode,
-                                    String description, String failingUrl) {
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
             myurl = view.getUrl();
             setContentView(R.layout.error);
             super.onReceivedError(view, errorCode, description, failingUrl);
         }
-
-
     }
 
     @Override
@@ -143,86 +139,43 @@ webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
                 case KeyEvent.KEYCODE_BACK:
                     if (webview.canGoBack()) {
                         webview.goBack();
-                    }
-                    else {
-
+                    } else {
                         final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-
                         builder.setMessage(R.string.exit_app);
-
                         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-
                                 finish();
                             }
                         });
-
-                        builder.setNegativeButton("No", new DialogInterface.OnClickListener(){
+                        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-
-                                
                             }
                         });
-
                         final AlertDialog dialog = builder.create();
-
                         dialog.show();
                     }
                     return true;
             }
-
         }
         return super.onKeyDown(keyCode, event);
     }
 
-    
-    public void tryAgain(View v){
-
+    public void tryAgain(View v) {
         setContentView(R.layout.activity_main);
-        webview =(WebView)findViewById(R.id.webView);
-        spinner = (ProgressBar)findViewById(R.id.progressBar1);
+        webview = (WebView) findViewById(R.id.webView);
+        spinner = (ProgressBar) findViewById(R.id.progressBar1);
         webview.setWebViewClient(new CustomWebViewClient());
 
-webview.setWebChromeClient(new WebChromeClient() {
- private View mCustomView;
- private CustomViewCallback mCustomViewCallback;
- protected FrameLayout mFullscreenContainer;
-    
-    @Override
-    public void onShowCustomView(View view, CustomViewCallback callback) {
-    if (mCustomView != null) {
-        callback.onCustomViewHidden();
-            return;
-    }
-        mCustomView = view;
-        mCustomViewCallback = callback;
-        mFullscreenContainer = new FrameLayout(MainActivity.this);
-        mFullscreenContainer.addView(view, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        ViewGroup decor = (ViewGroup) getWindow().getDecorView();
-    decor.addView(mFullscreenContainer);
-        webview.setVisibility(View.GONE);
-getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-}
-    @Override
-    public void onHideCustomView() {
-     if (mCustomView == null) return;
-        ViewGroup decor = (ViewGroup) getWindow().getDecorView();
-        decor.removeView(mFullscreenContainer);
-        mFullscreenContainer = null;
-        mCustomView = null;
-        mCustomViewCallback.onCustomViewHidden();
-        webview.setVisibility(View.VISIBLE);
-getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
- }
-});
-webview.setOnLongClickListener(ignore -> true);
-webview.setLongClickable(false);
-webview.getSettings().setJavaScriptEnabled(true);
-webview.getSettings().setMediaPlaybackRequiresUserGesture(false);
-webview.getSettings().setDomStorageEnabled(true);
-webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.setWebChromeClient(new WebChromeClient());
+
+        webview.setOnLongClickListener(ignore -> true);
+        webview.setLongClickable(false);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
         webview.loadUrl(myurl);
     }
 }
